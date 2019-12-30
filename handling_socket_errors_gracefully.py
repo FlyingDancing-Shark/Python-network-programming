465034d71921f837eb4586c8eeb5be0c5d7fc6b5
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-30 15:49:00 +0800

    Update handling_socket_errors_gracefully.py

--- handling_socket_errors_gracefully.py
+++ handling_socket_errors_gracefully.py
@@ -1,4 +1,4 @@
 '''
 *********description*********
-
+this is use as a client socket creation and connection error handling framework, we add try-except block in each of socket operation processes, you can use these  in your application logic
 '''
