4a682d5a962d54e9b2fdba5b7090fdc077fdb107
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-30 11:14:41 +0800

    Update miscellaneous_network_wrapper.py

--- miscellaneous_network_wrapper.py
+++ miscellaneous_network_wrapper.py
@@ -5,3 +5,10 @@ packed, fixed 4-bytes length hexadecimal notation strings (vice versa),
 query time from NTP server.....
 '''
 
+import socket
+from binascii import hexlify
+
+test_readable_ip = ['192.168.0.1', '8.8.8.8', '255.255.255.0']
+test_server_name = 'www.facebook.com'
+test_server_name_2 = 'www.vmofeuowlskqa.com'
+
