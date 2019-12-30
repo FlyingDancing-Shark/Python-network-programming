2856fe618f0ad065e2329390d43d6fcf740bc395
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-30 11:08:50 +0800

    Update miscellaneous_network_wrapper.py

--- miscellaneous_network_wrapper.py
+++ miscellaneous_network_wrapper.py
@@ -1,5 +1,7 @@
 '''
 *********description*********
-
+get host name, get remote server ip address, convert dotted decimal notation strings to 
+packed, fixed 4-bytes length hexadecimal notation strings (vice versa), 
+query time from NTP server.....
 '''
 
