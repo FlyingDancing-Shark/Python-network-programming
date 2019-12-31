b51c2a9116e297cf9c7e1872be919a2bb3273b09
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2019-12-31 14:31:34 +0800

    Update handling_socket_errors_gracefully.py

--- handling_socket_errors_gracefully.py
+++ handling_socket_errors_gracefully.py
@@ -27,9 +27,12 @@ def main():
 	# see another version "single_error_handling_of_socket.py"
 	try:
 		client_S = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		
 		receive_buffer_size = client_S.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
 		print "Receive Buffer Size:  %d" % receive_buffer_size
-		
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 65536)
+		receive_buffer_size = client_S.getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
+		print "Receive Buffer Size[adjustment]:  %d" % receive_buffer_size
 		# client_S.settimeout(time_out)
 	except socket.error, err:
 		print "Fail to create socket:  %s" % err
