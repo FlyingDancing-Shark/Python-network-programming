99731e2b70507620b0bb601ba8d5979906f65555
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-08 16:02:34 +0800

    Create PyNetcat_Utility_Knife.py

---
+++ PyNetcat_Utility_Knife.py
@@ -0,0 +1,8 @@
+'''
+*********description*********
+this python program implement most functionality similar to the Netcat using multi-thread 
+worker, including file upload, command execution remotely,  reverse back door shell, 
+I plan to add some more robust features in the future,such as disable OS firewall rule, 
+terminate anti-virus process.....
+'''
+
