08a452b3e34cf4028e323a3e5411022fccf543e9
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-09 11:18:18 +0800

    Update PyNetcat_Utility_Knife.py

--- PyNetcat_Utility_Knife.py
+++ PyNetcat_Utility_Knife.py
@@ -22,12 +22,59 @@ upload_file_path = ""
 port            = 0
 
 
-def usage():
+def print_usage():
 	
-  	
+  	sys.exit(0)
+	
 
 def main():
 	
+	global listen
+	global port
+	global execute
+	global command
+	global upload_file_path
+	global PyNetCat_server
 	
 	print "[*********] NetCat rewrite with Python [**************]"
+	
+	# sys.argv[0] = PyNetcat_Utility_Knife.py
+	# sys.argv[1] =
+	# sys.argv[2] = 
+	if not len(sys.argv[1:]):
+		print_usage()
+	
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hle:P:p:cu",\
+			["help", "listen", "execute", "target", "port", "command", "upload"])
+	except getopt.GetoptError as err:
+		print str(err)
+		print_usage()
+	
+	# here, "o" is command-line option (i.e., specify by user as -u or --upload), 
+	#       "a" its value (i.e., specify by user as c:\\target.exe)
+	# however, some option did not require associated value, so we just switch global status
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			print_usage()
+		elif o in ("-l", "--listen"):
+			listen  = True
+		elif o in ("-e", "--execute")"
+			execute = a
+		elif o in ("-c", "--commandshell"):
+			command = True
+		elif o in ("-u", "--upload"):
+			upload_file_path = a
+		elif o in ("-t", "--target"):
+				PyNetCat_server = a
+		elif o in ("-p", "--port"):
+				port = int(a)
+		else:
+			assert False, "Fail To Parsing Option, please check your typo"
+			
+	
+
+	
+	
+	
 	
