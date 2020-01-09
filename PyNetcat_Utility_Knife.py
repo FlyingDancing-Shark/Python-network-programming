4fd8a20cddde8796ea4f8841597c6dee0b6a6e36
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-10 02:01:41 +0800

    Update PyNetcat_Utility_Knife.py

--- PyNetcat_Utility_Knife.py
+++ PyNetcat_Utility_Knife.py
@@ -62,7 +62,7 @@ def main():
 		print_usage()
 	
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:c:s:r:",\
+		opts, args = getopt.getopt(sys.argv[1:], "hle:t:p:c:sr:",\
 			["help", "listen", "execute", "target", "port", "commandshell", "sendfilepath", "recvfilepath"])
 	except getopt.GetoptError as err:
 		print str(err)
