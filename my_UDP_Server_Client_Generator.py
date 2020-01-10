b0a625fe0f0c64deb8d8b5ca33f80e6addf3f9a4
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-10 23:42:24 +0800

    Update my_UDP_Server_Client_Generator.py

--- my_UDP_Server_Client_Generator.py
+++ my_UDP_Server_Client_Generator.py
@@ -44,7 +44,10 @@ if (2 <= len(sys.argv) <= 3) and (sys.argv[1] == 'server'):
 		# 
 		if random.randint(0, 1):
 			print '\n\tThe client at', address, 'says:', repr(data)
-			s.sendto('--------Your data was %d bytes--------' % len(data), address)
+			reply_msg = data[0:5] + "--------Your data was %d bytes--------" % len(data)
+			s.sendto(reply_msg, address)
+			
+			# s.sendto('--------Your data was %d bytes--------' % len(data), address)
 		
 	
 		# if generate number 0, then simulate we dropped packet due to network 
@@ -70,17 +73,37 @@ elif (len(sys.argv) == 3) and (sys.argv[1] == 'client'):
 	# internet_delay = 0.3
 	resend = 0
 	stay_under_max_delay = 0
+	repeat = False
+	seq_num = 0
+	previous_seq_num = 0 
 	
 	while True:
 		time.sleep(1)
-		s.send('--------This is another client message--------') 
+		
+		# when resend duplicate packet, using previously ID
+		if repeat:
+			duplicate_msg = str(previous_seq_num) + "--------DUPLICATE client message--------"
+			s.send(duplicate_msg)
+			
+		else:
+			# when sending new packet, generate new ID
+			seq_num = random.randint(10000, 99999)
+			
+			# save current ID for possible later use
+			previous_seq_num = seq_num
+			msg = str(seq_num) + "--------NEW client message--------"
+			s.send(msg)
+			# s.send('--------This is another client message--------') 
 		print '\n\t--------Waiting up to', local_delay, 'seconds for a reply, the', resend, 'th resend--------'
 		s.settimeout(local_delay)
 		# s.settimeout(internet_delay)
 		
 		try:
 			data = s.recv(MAX_RECV)
+		# we need to resend, so flag repeat 
 		except socket.timeout:
+			
+			repeat = Ture
 			local_delay *= 2
 			
 			# if we resend over four times, stop increase waiting time or 
@@ -105,6 +128,8 @@ elif (len(sys.argv) == 3) and (sys.argv[1] == 'client'):
 		# if server reply before current timeout value, exit loop, 
 		# print message receiving from server	
 		else:
+			# receive
+			if data[0:5] == str(seq_num):
 			if resend <= 3:
 				local_delay = 0.01
 				print '\n\tGet reply before %d retries, now reset timeout to: %.2f seconds' % (resend, local_delay) 
@@ -112,6 +137,7 @@ elif (len(sys.argv) == 3) and (sys.argv[1] == 'client'):
 			stay_under_max_delay = 0
 			# break
 			print '\n\tThe server says: ', repr(data)
+			repeat = False
 			
 	# if we put this print into "else" block, and comment "break", then we can test 
 	# along with "time.sleep(random.randint(5, 15))"
