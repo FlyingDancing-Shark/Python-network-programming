669d0191b879aa72fc356e3c21c6acb2cddb2067
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-13 13:51:08 +0800

    Update my_UDP_Server_Client_Generator.py

--- my_UDP_Server_Client_Generator.py
+++ my_UDP_Server_Client_Generator.py
@@ -65,9 +65,9 @@ if (2 <= len(sys.argv) <= 3) and (sys.argv[1] == 'server'):
 			else:
 			'''	
 			print '\n\treceive client message: ', repr(client_request)
-				reply_to_client = client_request[0:5] + "--------Server Reply--------"
+			reply_to_client = client_request[0:5] + "--------Server Reply--------"
 			print '\n\tNow reply client: ', repr(reply_to_client)	
-				s.sendto(reply_to_client, address)
+			s.sendto(reply_to_client, address)
 		
 		
 		# if generate number 0, then simulate we dropped packet due to network 
