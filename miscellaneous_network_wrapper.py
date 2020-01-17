7a41a5fce8c996fac77a33fa50cf3aeb4c19021b
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-01-17 14:39:51 +0800

    Update miscellaneous_network_wrapper.py

--- miscellaneous_network_wrapper.py
+++ miscellaneous_network_wrapper.py
@@ -59,12 +59,12 @@ def query_MTU():
             s.send('#' * mtu)
         except socket.error, err:
             print 'fail to send due to:  %s' % err
-            print 'MTU is: %d' % mtu
+            print 'MTU is: %d' % mtu - 1
             break
         else:
             mtu += 1
     
-    return mtu
+    return mtu - 1
 
 
     
