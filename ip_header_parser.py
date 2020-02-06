46718eeef09859e57b2266f00e47422a48ee4a98
FlyingDancing-Shark <58820486+FlyingDancing-Shark@users.noreply.github.com>
2020-02-06 23:30:18 +0800

    Update ip_header_parser.py

--- ip_header_parser.py
+++ ip_header_parser.py
@@ -1,6 +1,9 @@
 """
 description
-
+this program implemented a simple sniffer using raw socket,
+the packets it captured include IP header and anything above the L3,
+the "IP_hdr" class serve as an IP header parser and container for storage ourselves
+human-readable IP header fields format that parsed from raw packets..........
 """
 
 from ctypes import *
@@ -12,12 +15,12 @@ import struct
 host_address =  "192.168.3.113"
 
 
-class IP_hdr(header):
+class IP_hdr(Structure):
 
     _fields_ = [
         ("ihl", c_ubyte,  4),      ("version", c_ubyte,  4),  ("tos", c_ubyte),
-        ("len", c_ushort),         ("id", c_ushort),          ("offset", c_ushort)
-        ("ttl", c_ubyte),          ("protocol_num", c_ubyte), ("sum", c_ushort)
+        ("len", c_ushort),         ("id", c_ushort),          ("offset", c_ushort),
+        ("ttl", c_ubyte),          ("protocol_num", c_ubyte), ("sum", c_ushort),
         ("src", c_ulong),          ("dst", c_ulong)
     ]
 
@@ -49,6 +52,7 @@ sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
 if os.name == 'nt':
     sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    print "\n\tturn ON promiscuous mode of network adapter....."
 
 try:
 
@@ -56,9 +60,10 @@ try:
 
         raw_packet = sniffer.recvfrom(65535)[0]
         ip_header = IP_hdr(raw_packet[0:20])
-        print "Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
+        print "Protocol: %s %s ---> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address)
 
 except KeyboardInterrupt:
     if os.name == "nt":
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        print "\n\tturn OFF promiscuous mode of network adapter....."
     sys.exit(0)
